0131377db052f1204ed81bd1b460201316dbbab6
Enbvhir <Enbvhir@outlook.com>
2025-12-03 13:04:37 +0800

    增加了幽灵的闪烁效果

--- ui/play/ghost.gd
+++ ui/play/ghost.gd
@@ -1,12 +1,23 @@
 extends Sprite2D
 
-@export var amplitude := 10
-@export var speed := 2.0
+@export var amplitude := 20
+@export var fade_duration := 3.0
+
 var base_y
+var time_passed := 0.0
 
 func _ready():
 	base_y = position.y
 
 func _process(delta):
-	if is_in_group("ghosts"):
-		position.y = base_y + sin(Time.get_ticks_msec() / 300.0) * amplitude
+	time_passed += delta
+
+	position.y = base_y + sin(Time.get_ticks_msec() / 300.0) * amplitude
+
+	var fade_phase = sin((time_passed / fade_duration) * PI) ** 2
+
+	var min_alpha: float = 0.2
+	var max_alpha: float = 1.0
+	var alpha: float = lerp(min_alpha, max_alpha, fade_phase)
+
+	self.modulate.a = alpha
